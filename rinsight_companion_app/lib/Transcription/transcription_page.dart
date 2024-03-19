ec450a8f52debeb5be1f6558ab84d741c4c93ab4
aezrapour <86795185+aezrapour@users.noreply.github.com>
2024-03-19 18:51:48 -0400

    Branching Out: Committed to the Script of Success

--- rinsight_companion_app/lib/Transcription/transcription_page.dart
+++ rinsight_companion_app/lib/Transcription/transcription_page.dart
@@ -1,21 +1,38 @@
 import 'package:flutter/material.dart';
 
-class TransScreen extends StatelessWidget {
-  const TransScreen({super.key});
+class TransScreen extends StatefulWidget {
+  const TransScreen({Key? key}) : super(key: key);
+
+  @override
+  _TransScreenState createState() => _TransScreenState();
+}
+
+class _TransScreenState extends State<TransScreen> {
+  // Hardcoded list of transcriptions for demonstration
+  final List<String> _transcriptions = [
+    "Hello, how are you today?",
+    "I'm fine, thank you!",
+    "Isn't the weather lovely today?",
+    "Yes, it's perfect for a walk in the park.",
+    // Add more items as needed
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Second Route'),
+        title: Text('Transcriptions'),
+        centerTitle: true,
       ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            // Navigate back to first route when tapped.
-          },
-          child: const Text('Go back!'),
-        ),
+      body: ListView.builder(
+        itemCount: _transcriptions.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            leading: Icon(Icons.mic, color: Colors.blueAccent),
+            title: Text(_transcriptions[index]),
+            // Customize each ListTile as needed
+          );
+        },
       ),
     );
   }
